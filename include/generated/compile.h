8391c5fcb9c93bbb121a2c2164e295ac4d455a1e
Czar Dixon <rbheromax@rbheromax-dell.(none)>
2013-02-12 20:24:14 -0500

    Just uploading src that is tested Compiles just fine ;)

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Feb 12 17:30:03 EST 2013"
+#define LINUX_COMPILE_BY "rbheromax"
+#define LINUX_COMPILE_HOST "rbheromax-dell"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
